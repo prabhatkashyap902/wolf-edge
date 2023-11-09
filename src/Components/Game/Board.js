4e06177e2690bb2cdb07ad77adcf4a1abad7d7b6
unknown <10p14ed0078@gmail.com>
2023-11-10 00:22:08 +0530

    make move done

--- src/Components/Game/Board.js
+++ src/Components/Game/Board.js
@@ -3,25 +3,24 @@ import Square from "./Square";
 import { useDispatch, useSelector } from 'react-redux';
 import { setPlayer1Address, setPlayer2Address } from "../Redux/DataSlice";
 
-function Board({gameId,player,comingFrom,player2Joined}) {
+function Board({gameId,comingFrom,player2Joined}) {
 	const [squares, setSquares] = useState(Array(9).fill(null));	
 	const contract = useSelector((state) => state.data.contract);
-	const provider=useSelector((state)=>state.data.provider)
 	const webSocketContract = useSelector((state) => state.data.webSocketContract);
 	const player1Address=useSelector((state)=>state.data.player1Address)
 	const player2Address=useSelector((state)=>state.data.player2Address)
-	const[player1Play,setPlayer1Play]=useState(false)
 	const[rounds, setRounds]=useState(0)
 	const[whichPlayer,setWhichPlayer]=useState('')
 	const[makeUnClickable,setMakeUnClickable]=useState(false)
 	
-const dispatch=useDispatch()
+	const dispatch=useDispatch()
+
 	useEffect(()=>{
 		
 		console.log(comingFrom)
-		if(comingFrom==='start')setWhichPlayer(player1Address)
-		else setWhichPlayer(player2Address)
-
+		if(comingFrom==='start'){setWhichPlayer(player1Address); setMakeUnClickable(false)}
+		else {setWhichPlayer(player2Address); setMakeUnClickable(true)}
+	
 		const handleJoinedGame = (gameId, player2) => {
 			console.log("BoardJoinedGame")
 			console.log(player2)
@@ -35,18 +34,10 @@ const dispatch=useDispatch()
 		}
 		const handleGameStarted = (gameId, player1, betAmount) => {
 			console.log("BoardGameStarted")
-			// setPlayer1Play(true)
 			console.log(player1)
-			// setWhichPlayer(player1)
 		}
-		const handleMadeMove= async(gameId,x,y, player) => {
-			// console.log("BoardMadeMove")
-			// console.log(`MadeMove of gameID ${gameId} of x:${x} and y:${y} by ${player}`)
-			// console.log(whichPlayer)
-			// console.log(player1Address)
-			// console.log(player2Address)
-			// Update the board state based on the move made
-      // const newSquares = [...squares];
+		const handleMadeMove= (gameId,x,y, player) => {
+			
 			setSquares((prevSquares) => {
 				// Create a new array from the previous squares
 				const newSquares = [...prevSquares];
@@ -57,25 +48,24 @@ const dispatch=useDispatch()
 				// Return the updated squares array
 				return newSquares;
 			});
-      // setSquares(newSquares);
-
-      // Toggle whichPlayer based on the move made
-      setWhichPlayer(player === player1Address ? player2Address : player1Address);
-      // setIsXNext(player !== player1Address); // If player 1 made a move, it's now player 2's turn, and vice versa
-      setMakeUnClickable(false); // Re-enable clicking after the move is handled
-    
+			setRounds(rounds=>rounds+1)
+			// Toggle whichPlayer based on the move made
+			setWhichPlayer(player === player1Address ? player2Address : player1Address);
+			setMakeUnClickable(comingFrom==='start'?(player===player1Address?true:false):(player===player2Address?true:false)); // Re-enable clicking after the move is handled
+	
 			
 			
 		};
+		const handleWinner= (gameId, winner, reward) => {
+			console.log("BoardHaveWinner")
+			console.log(`hey this is gameId: ${gameId} and the winner is ${winner} and the amount is ${reward}`)
+
+		}
 
 		webSocketContract.on('JoinedGame',handleJoinedGame)
 		webSocketContract.on('GameStarted',handleGameStarted)
 		webSocketContract.on('MadeMove', handleMadeMove);
-		
-		// webSocketContract.on('HaveWinner', (gameId, winner, reward) => {
-		// 	console.log("BoardHaveWinner")
-			
-		// });
+		webSocketContract.on('HaveWinner',handleWinner)
 		// webSocketContract.on('GameCancelled',(gameId,player1)=>{
 		// 	console.log("BoardGameCancelled")
 		// });
@@ -85,6 +75,7 @@ const dispatch=useDispatch()
 			webSocketContract.off('JoinedGame', handleJoinedGame);
 			webSocketContract.off('GameStarted', handleGameStarted);
 			webSocketContract.off('MadeMove', handleMadeMove);
+			webSocketContract.off('HaveWinner', handleWinner);
 			// ... unsubscribe from other events ...
 		};
 
@@ -97,27 +88,8 @@ const dispatch=useDispatch()
 			try {
 				
 			
-				// webSocketContract.on('MadeMove', (gameId,x,y, player) => {
-				// 	console.log("BoardMadeMove")
-				// 	console.log(`MadeMove of gameID ${gameId} of x:${x} and y:${y} `)
-				// 	if(player==player1Address){
-						
-				// 		setWhichPlayer(player2Address)
-						
-				// 		setSquares(squares);
-				// 	}
-				// 	else{
-				// 		setWhichPlayer(player1Address)
-						
-				// 		setSquares(squares);
-				// 	}
-				// 		console.log(squares)
-				// 		setSquares(squares);
-				// 		setMakeUnClickable(false)
-					
-				// });
+				
 				const transaction = await contract.makeMove(x,y,gameId);
-				// const receipt = await provider.waitForTransaction(transaction.hash);
 				console.log(`Transaction mined with ${transaction} confirmations`);
 					
 				
@@ -147,14 +119,14 @@ const dispatch=useDispatch()
 
 
 		if (!squares[i]) {
-      try {
-        setMakeUnClickable(true); // Disable further clicks until the move is processed
-        makeMove(Math.floor(i / 3), i % 3, squares);
-      } catch (error) {
-        console.error("Failed to make a move:", error);
-        setMakeUnClickable(false); // Re-enable clicking if there's an error
-      }
-    }
+			try {
+				setMakeUnClickable(true); // Disable further clicks until the move is processed
+				makeMove(Math.floor(i / 3), i % 3, squares);
+			} catch (error) {
+				console.error("Failed to make a move:", error);
+				setMakeUnClickable(false); // Re-enable clicking if there's an error
+			}
+		}
 
 		
 	};
@@ -173,6 +145,8 @@ const dispatch=useDispatch()
 		for (let i = 0; i < lines.length; i++) {
 			const [a, b, c] = lines[i];
 			if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+				console.log("winner mil gya")
+				// setMakeUnClickable(true)
 				return squares[a];
 			}
 		}
@@ -180,11 +154,11 @@ const dispatch=useDispatch()
 	}
 
 	const renderSquare = (i) => {
-		return <Square value={squares[i]} onClick={() =>handleClick(i)} makeUnClickable={makeUnClickable} setMakeUnClickable={setMakeUnClickable} />;
+		return <Square value={squares[i]} onClick={() =>handleClick(i)} makeUnClickable={makeUnClickable} />;
 	};
 
 	const winner = calculateWinner(squares);
-	// const status = winner ? `Winner: ${winner}` : `Next Move: ${isXNext ? "X" : "O"}`;
+	// const status = winner ? `Winner: ${winner}` : `Next	 Move: ${isXNext ? "X" : "O"}`;
 
 	return (
 		<div className="flex flex-col justify-center text-center">
@@ -204,7 +178,7 @@ const dispatch=useDispatch()
 					</div>
 				))}
 				<div className="col-span-3 mt-4 text-center">
-					
+				{comingFrom==="start"?(rounds%2===0?"Your Turn":"Let the Player 2 make a move"):(rounds%2===0?"Let the Player 1 to make a move":"Your Turn")}
 				</div>
 			</div>
 		</div>
--- src/Components/Game/Square.js
+++ src/Components/Game/Square.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-function Square({ value, onClick,makeUnClickable,setMakeUnClickable  }) {
+function Square({ value, onClick,makeUnClickable  }) {
   return (
     <button className="w-full h-full text-4xl bg-white border border-gray-400" onClick={onClick} disabled={makeUnClickable}>
       {value}
